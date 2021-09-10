ad73e28338d65c40241ce5b6dfa0db8da4fa7fd4
Saad <saadkamal219@gmail.com>
2021-09-10 14:31:58 +0600

    password

--- input.c
+++ input.c
@@ -1,1 +1,51 @@
-#inlcude<stdio.h>
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+main()
+{
+int i,n,lc=0,uc=0,dc=0,sc=0;
+char pass[100], user[100];
+LOOP:
+printf("\nEnter the password:");
+gets(pass);
+n=strlen(pass);
+if(n>=8){
+
+for(i=0;i<n;i++)
+{
+    if(pass[i]==' ')
+    {
+    printf("\n\tYou can't give any space \n\tTry again ");goto LOOP;
+    }
+else
+    if(isalpha(pass[i]))
+    {
+    if(isupper(pass[i]))
+    uc++;
+    else
+    lc++;
+    }
+    else if(isdigit(pass[i]))
+    dc++;
+    else if((ispunct(pass[i]))&&(isgraph(pass[i])))
+    sc++;
+}
+}
+else
+    {printf("\n\tEnter atleast 8 characters!\n");
+    goto LOOP;}
+if(lc>0 )
+   {
+    if(uc>0)
+    {
+        if(sc>0)
+         {
+            if(dc>0)
+         {
+          printf("\n\tYour account has been created successfully\n");exit(1);
+         }else printf("\n\tDigit is missing in your password\n\tTry again!");goto LOOP;
+      }else printf("\n\tSpecial character is missing in your password\n\tTry again!");goto LOOP;
+    }else printf("\n\tUppercase is missing in your password\n\tTry again!");goto LOOP;
+   }else printf("\n\tLowercase is missing in your password\n\tTry again!");goto LOOP;
+
+}
